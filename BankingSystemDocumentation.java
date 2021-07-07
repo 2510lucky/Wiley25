b2ff939ff26d8eaa7bfbc4e73ecf562c0357d01d
Lavanya Agrawl <luckygargroxx2510@gmail.com>
2021-07-07 14:41:35 +0530

    first commit

---
+++ BankingSystemDocumentation.java
@@ -0,0 +1,37 @@
+/*
+Banks
+    * Name
+    * Branch
+    * Branch Address
+
+* N users
+    * Name
+    * ID
+    * Number
+    * Address
+
+    * User Operations 
+        * Login Module
+        * Open account
+        * Balance Enquiry
+        * Account History
+        * Admin Portal
+        * Loan 
+        * Money Transfer
+        * Debit Card
+        * Credit Card
+        * Cheque Book
+
+* Bank
+    * Bank Employees
+        * Managers
+        * Normal Employees
+
+    * Transaction Records
+        * Deposite Record
+        * Withdraw Record
+        * Loan Record
+        * EMI Record
+
+*/
+
